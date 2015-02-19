6ef3c1845bf7c9e2f376b6a15370eade1dcdd3c4
Faizov Artur <Faizov@gmail.com>
2015-02-19 17:33:11 +0300

    удален мусор

--- Evolution_01/Program.cs
+++ Evolution_01/Program.cs
@@ -7,14 +7,12 @@ namespace Evolution_01
 {
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения. Новый первый коммит 5
-        /// </summary>
-        [STAThread]
+
+        //[STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
     }
